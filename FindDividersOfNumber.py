3373146ca51aaeec5caf104cab7f7fb0a0afa86b
Alexander <pchp250g8@tutamail.com>
2026-03-28 16:13:24 +0300

    Fixed Source Code

--- FindDividersOfNumber.py
+++ FindDividersOfNumber.py
@@ -4,7 +4,7 @@ import os
 os.system("cls")
 print("Input an integer positive number")
 ulNumber = int(input())
-print(f"The number {ulNumber} has the folowing dividers:")
+print(f"The number {ulNumber} has the folowing divisors:")
 for i in range(1, ulNumber + 1):
     if ulNumber % i == 0:
         print(i, end=" ")
